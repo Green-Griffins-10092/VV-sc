4d04d5b42bbc2ee59fc83895d8ece58c18d05305
David <dflores0818@gmail.com>
2017-07-10 19:49:05 -0700

    This is "the first teleOp"

---
+++ Griffins/src/main/java/org/firstinspires/ftc/griffins/FirstTeleop.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.griffins;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by dflor on 7/10/2017.
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class FirstTeleop extends OpMode {
+
+    RobotHardware robot;
+
+    @Override
+    public void init() {
+        robot = new RobotHardware();
+        robot.initialize(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double left;
+        double right;
+        double intake;
+
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+        intake = gamepad1.left_trigger;
+
+
+        robot.getLeftDrive().setPower(left);
+        robot.getRightDrive().setPower(right);
+        robot.getIntake().setPower(intake);
+
+        telemetry.addData("left", left);
+        telemetry.addData("right", right);
+        telemetry.addData("intake", intake);
+    }
+
+}
